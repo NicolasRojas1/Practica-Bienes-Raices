1ed888f356dee638b28432ab3f3b72c229984db6
NicolasRojas <nicrojasc6@gmail.com>
2024-08-06 18:16:08 -0500

    [UPDATE] darkmode completo y ajuste menu hamburguesa

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     eventListeners();
-
     darkMode();
 });
 
@@ -21,7 +20,6 @@ function eventListeners() {
 
     //Al darle clic ejecutara navegacionResponsive
     mobileMenu.addEventListener('click', navegacionResponsive);
-    mobileMenu.addEventListener('click', ajustarMenu)
 }
 
 function navegacionResponsive() {
@@ -30,27 +28,5 @@ function navegacionResponsive() {
     //Si tiene la clase mostrar la quita, si no la agrega
     navegacion.classList.toggle('mostrar');
 }
-//Este evento esta pendiente al cambio de pantalla y aplica la funcion de ajustarMenu
-window.addEventListener('resize', ajustarMenu);
 
-function ajustarMenu() {
-    const navegacion = document.querySelector('.navegacion');
-    const barra = document.querySelector('.barra');
 
-    // Verifica si barra y navegacion existen
-    if (barra) {
-        // Verifica si la pantalla es móvil
-        if (window.matchMedia("(max-width: 768px)").matches) {
-            if (navegacion.classList.contains('mostrar')) {
-                console.log('Menu desplegado en móvil');
-                barra.style.height = '32rem';
-            } else {
-                console.log('Menu cerrado en móvil');
-                barra.style.height = '12rem';
-            }
-        } else {
-            // Pantalla grande, restablece el height
-            barra.style.height = '';
-        }
-    }
-}
