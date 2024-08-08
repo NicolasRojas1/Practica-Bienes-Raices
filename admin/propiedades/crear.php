3d75d7fcce7b45c77138aa986ea386998c56b1c6
NicolasRojas <nicrojasc6@gmail.com>
2024-08-08 18:34:19 -0500

    [ADD] formulario crear.php

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,48 @@
         <h1>Crear</h1>
 
         <a href="/bienesraices/admin/index.php" class="boton boton-verde">Volver</a>
+
+        <form class="formulario" action="">
+            <fieldset>
+                <legend>Informacion General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripcion:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 2" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 2" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 2" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Nicolas</option>
+                    <option value="1">Yenni</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 <?php
