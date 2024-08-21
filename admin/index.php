0a9f4c4b68ba351ad0a2783095e015bcccb8df7e
NicolasRojas <nicrojasc6@gmail.com>
2024-08-21 18:57:10 -0500

    [ADD] plantilla para listar propiedades

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,29 @@
 
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="../imagenes/ef389dbc20790e559f04302065bd6881.jpg" class="imagen-tabla"> </td>
+                    <td>1200000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
