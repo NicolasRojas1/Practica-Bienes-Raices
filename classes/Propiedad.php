4fd7686fb4f8188ef665fc60c0211533a5679635
NicolasRojas <nicrojasc6@gmail.com>
2024-09-11 19:43:25 -0500

    [UPDATE] sanitizar datos en propiedad.php

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -6,6 +6,8 @@ class Propiedad {
 
     //DB, static solo para que se cree una vez, no es necesario crear varias conexiones
     protected static $db;
+    //Para el proceso de sanitizar, se crea un arreglo para identificar la forma de los datos
+    protected static $columnasDB = ['id', 'titulo', 'precio', 'imagen', 'descripcion', 'habitaciones', 'wc', 'estacionamiento', 'creado', 'vendedorId'];
     
     public $id;
     public $titulo;
@@ -17,6 +19,12 @@ class Propiedad {
     public $estacionamiento;
     public $creado;
     public $vendedorId;
+
+    //Definir la conexion a la DB
+    public static function setDB($database) {
+        //self hace referencia a los atributos estaticos de una misma clase
+        self::$db = $database;
+    }
 
     public function __construct($args = [])
     {
@@ -35,6 +43,10 @@ class Propiedad {
 
     public function guardar() {
 
+        //Sanitizar la entrada de los datos
+        $atributos = $this->sanitizarAtributos();
+        debuguear($atributos);
+
         //Insertar en la db
         $query = " INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ( '$this->titulo', '$this->precio', '$this->imagen', '$this->descripcion', '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->creado', '$this->vendedorId' ) ";
 
@@ -43,9 +55,31 @@ class Propiedad {
         debuguear($resultado);
     }
 
-    //Definir la conexion a la DB
-    public static function setDB($database) {
-        //self hace referencia a los atributos estaticos de una misma clase
-        self::$db = $database;
+    //Identifica y une los atributos de la DB a los ingresados
+    public function atributos() {
+        $atributos = [];
+        foreach(self::$columnasDB as $columna) {
+            //Para que igner el id, ya que la db lo agrega automaticamente
+            if($columna === 'id') continue; 
+            //Se van mapeando los atributos con las columnas de la db
+            $atributos[$columna] = $this-> $columna;
+        }
+        return $atributos;
     }
+
+    //Sanitiza cada atributo, para evitar codigo malicioso en la app
+    public function sanitizarAtributos() {
+        //Llamo al metodo que mapea las columnas con el objeto en memoria que se tiene
+        $atributos = $this-> atributos();
+        $sanitizado = [];
+
+        //Asi tomo tanto el nombre del atributo y el valor que se ingresa, ej: 'titulo' como key, 'casa en las montañas' como value
+        foreach($atributos as $key => $value) {
+            //Con escape_string sanitizo los datos del value, que son los que me interesan guardar en la db
+            $sanitizado[$key] = self::$db->escape_string($value);
+        }
+
+        return $sanitizado;
+    }
+    
 }
